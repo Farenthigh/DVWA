47a720d21f8df1c88437bf540da07e478fddcffb
Farenthigh <naxx.iiax@gmail.com>
2026-08-11 15:25:40 +0700

    Disable startup error display for improved security

--- vulnerabilities/bac/source/impossible.php
+++ vulnerabilities/bac/source/impossible.php
@@ -4,6 +4,7 @@ if (!defined('DVWA_WEB_PAGE_TO_ROOT')) {
 }
 error_reporting(E_ALL);
 ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
 ini_set('log_errors', '1');
 
 // Initialize variables
