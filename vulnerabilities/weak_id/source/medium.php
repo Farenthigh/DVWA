c56a4c79bfb85a35eaaa94c88f7235b0bd1fc79e
Farenthigh <naxx.iiax@gmail.com>
2026-08-11 15:03:30 +0700

    Enhance session cookie security by setting attributes: secure, HttpOnly, and SameSite

--- vulnerabilities/weak_id/source/medium.php
+++ vulnerabilities/weak_id/source/medium.php
@@ -4,6 +4,16 @@ $html = "";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	$cookie_value = time();
-	setcookie("dvwaSession", $cookie_value);
+	setcookie(
+    "dvwaSession",
+    $cookie_value,
+    [
+        'expires' => 0,
+        'path' => '/',
+        'secure' => true,
+        'httponly' => true,
+        'samesite' => 'Lax'
+    ]
+);
 }
 ?>
