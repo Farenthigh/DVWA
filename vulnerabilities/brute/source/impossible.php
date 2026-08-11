4ebd3c8fc4b636a8405248c9bdd59c2b06f4816e
Farenthigh <naxx.iiax@gmail.com>
2026-08-11 10:02:21 +0700

    Use random_int instead of rand for sleep duration in login failure handling

--- vulnerabilities/brute/source/impossible.php
+++ vulnerabilities/brute/source/impossible.php
@@ -79,7 +79,7 @@ if( isset( $_POST[ 'Login' ] ) && isset ($_POST['username']) && isset ($_POST['p
 		$data->execute();
 	} else {
 		// Login failed
-		sleep( rand( 2, 4 ) );
+		sleep( random_int( 2, 4 ) );
 
 		// Give the user some feedback
 		$html .= "<pre><br />Username and/or password incorrect.<br /><br/>Alternative, the account has been locked because of too many failed logins.<br />If this is the case, <em>please try again in {$lockout_time} minutes</em>.</pre>";
